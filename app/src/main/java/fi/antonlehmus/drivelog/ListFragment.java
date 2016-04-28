a87bd875dc454696804a13e1653f92347a45d44f
Anton Lemus <anton.lehmus@gmail.com>
2016-04-28 18:59:52 +0300

    add swipe refres

--- app/src/main/java/fi/antonlehmus/drivelog/ListFragment.java
+++ app/src/main/java/fi/antonlehmus/drivelog/ListFragment.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,18 +21,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class ListFragment extends Fragment {
+public class ListFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(this);
+
+        /**
+         * Showing Swipe Refresh animation on activity create
+         * As animation won't start on onResume, post runnable is used
+         */
+        swipeRefreshLayout.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        swipeRefreshLayout.setRefreshing(true);
+
+                                        refresh();
+                                    }
+                                }
+        );
+
         return view;
     }
-    @Override
-    public void onResume(){
 
+    @Override
+    public void onRefresh() {
+        refresh();
+    }
+
+    //allows both modes
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if(isVisibleToUser) {
+            Activity a = getActivity();
+            if(a != null) a.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
+        }
+    }
+
+    private void refresh(){
         //read all journeys from database
         FlowQueryList<Journey> list = new FlowQueryList<>(SQLite.select().from(Journey.class).orderBy(Journey_Table.dateTime,false));
 
@@ -44,18 +78,7 @@ public class ListFragment extends Fragment {
         listView.setAdapter(adapter);
 
         adapter.addAll(list);
-
-        super.onResume();
-    }
-
-    //allows both modes
-    @Override
-    public void setUserVisibleHint(boolean isVisibleToUser) {
-        super.setUserVisibleHint(isVisibleToUser);
-        if(isVisibleToUser) {
-            Activity a = getActivity();
-            if(a != null) a.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
-        }
+        swipeRefreshLayout.setRefreshing(false);
     }
 
 }
